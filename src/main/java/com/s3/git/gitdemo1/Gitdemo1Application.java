21bbc2427108b8b0566e075b96ae19d3ceed1604
dilipmaharjan <dilipmaharjan76@gmail.com>
2021-01-04 23:41:05 -0600

    changes for requirement something

--- src/main/java/com/s3/git/gitdemo1/Gitdemo1Application.java
+++ src/main/java/com/s3/git/gitdemo1/Gitdemo1Application.java
@@ -1,13 +1,19 @@
 package com.s3.git.gitdemo1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Gitdemo1Application {
+public class Gitdemo1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Gitdemo1Application.class, args);
 	}
 
+
+	@java.lang.Override
+	public void run(java.lang.String... args) throws Exception {
+
+	}
 }
